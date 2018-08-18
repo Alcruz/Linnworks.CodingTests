c3a8d363464bc2e02c6ab5c477626398a52edbf3
Alvin Jonel de la Cruz <alvinj.delacruz@gmail.com>
2018-08-18 16:06:01 -0400

    Refactoring ShortestPath solver

--- src/Linnworks.CodingTests.Part3/Domain/FindShorestPath.cs
+++ src/Linnworks.CodingTests.Part3/Domain/FindShorestPath.cs
@@ -5,21 +5,21 @@ using System.Linq;
 
 namespace Linnworks.CodingTests.Part3.Domain
 {
-	internal class FindShorestPath
+	internal class FindShorestPath<T> where T : State<T>
 	{
 		private Node startNode;
 		private Node goalNode;
 
-		public FindShorestPath(World.Cell startLoc, World.Cell endLoc, Func<Location, IEnumerable<World.Cell>> successorsFunc)
+		public FindShorestPath(State<T> startLoc, State<T> endLoc, Func<State<T>, IEnumerable<State<T>>> successorsFunc)
 		{
 			this.startNode = new Node(startLoc);
 			this.goalNode = new Node(endLoc);
 			SuccessorsFunc = successorsFunc;
 		}
 
-		public Func<Location, IEnumerable<World.Cell>> SuccessorsFunc { get; }
+		public Func<State<T>, IEnumerable<State<T>>> SuccessorsFunc { get; }
 
-		internal Location[] Run()
+		internal State<T>[] Run()
 		{
 
 			var node = this.startNode;
@@ -30,25 +30,25 @@ namespace Linnworks.CodingTests.Part3.Domain
 			while (true)
 			{
 				if (!frontierSet.Any())
-					return new Location[0];
+					return new State<T>[0];
 
 				node = frontier.Max;
 
 				frontier.Remove(node);
 				frontierSet.Remove(node);
 
-				if (node.Equals(goalNode))
+				if (node.State.Equals(goalNode.State))
 					return Path(node);
 
 				explored.Add(node);
-				foreach (var child in SuccessorsFunc.Invoke(node.Location).Select(successor => new Node(successor, node)))
+				foreach (var child in SuccessorsFunc.Invoke(node.State).Select(successor => new Node(successor, node)))
 				{
 					if (!explored.Contains(child) && !frontierSet.Contains(child) )
 					{
 						frontier.Add(child);
 						frontierSet.Add(child);
 					}
-					else if (frontierSet.TryGetValue(child, out Node foundNode) && child.Cost > foundNode.Cost)
+					else if (frontierSet.TryGetValue(child, out Node foundNode) && child.PathCost > foundNode.PathCost)
 					{
 						frontier.Remove(foundNode);
 						frontierSet.Remove(foundNode);
@@ -60,65 +60,35 @@ namespace Linnworks.CodingTests.Part3.Domain
 			}
 		}
 
-		private Location[] Path(Node node) => node.Reverse().Select(n => n.Location).ToArray();
-		private class Node : IEquatable<Node>, IEnumerable<Node>
+		private State<T>[] Path(Node node) => node.Reverse().Select(n => n.State).ToArray();
+		private class Node : IEnumerable<Node>
 		{
 			private static Lazy<Comparer<Node>> LazyComparer = new Lazy<Comparer<Node>>(() => {
 				return Comparer<Node>.Create((node1, node2) =>
 				{
-					if (node1.Cost.CompareTo(node2.Cost) != 0)
-						return node1.Cost.CompareTo(node2.Cost);
-					else if (node1.Location.X.CompareTo(node2.Location.X) != 0)
-						return node1.Location.X.CompareTo(node2.Location.X);
-					else if (node1.Location.Y.CompareTo(node2.Location.Y) != 0)
-						return node1.Location.Y.CompareTo(node2.Location.Y);
-
-					return 0;
+					if (node1.PathCost.CompareTo(node2.PathCost) != 0)
+						return node1.PathCost.CompareTo(node2.PathCost);
+					
+					return node1.State.CompareTo(node2.State);
 				});
 			});
 
-			public Node(World.Cell location, Node predecessor = null)
+			public Node(State<T> location, Node predecessor = null)
 			{
-				Location = location;
+				State = location;
 				Predecessor = predecessor;
 			}
 
 			public static Comparer<Node> Comparer => LazyComparer.Value;
-			public World.Cell Location { get; }
+			public State<T> State { get; }
 			public Node Predecessor { get; }
-			public int Cost
+			public int PathCost
 			{
 				get
 				{
-					if (Location.Passability == 0) return int.MinValue;
+					if (State.Cost == 0) return int.MinValue;
 
-					return Location.Passability - (Predecessor?.Cost ?? 0);
-				}
-			}
-
-			public bool Equals(Node other)
-			{
-				if (other == null) return false;
-				return Location.X == other.Location.X
-				 && Location.Y == other.Location.Y;
-			}
-
-			public override bool Equals(object obj)
-			{
-				if (ReferenceEquals(null, obj)) return false;
-				if (ReferenceEquals(this, obj)) return true;
-				if (obj.GetType() != GetType()) return false;
-				return Equals(obj as Node);
-			}
-
-			public override int GetHashCode()
-			{
-				unchecked
-				{
-					var hashCode = 13;
-					hashCode = (hashCode * 397) ^ Location.X;
-					hashCode = (hashCode * 397) ^ Location.Y;
-					return hashCode;
+					return State.Cost - (Predecessor?.PathCost ?? 0);
 				}
 			}
 
@@ -147,13 +117,22 @@ namespace Linnworks.CodingTests.Part3.Domain
 
 			public override bool Equals(Node x, Node y)
 			{
-				return x.Equals(y);
+				return x.State.Equals(y.State);
 			}
 
 			public override int GetHashCode(Node obj)
 			{
-				return obj.GetHashCode();
+				return obj.State.GetHashCode();
 			}
 		}
+	}
+
+	public abstract class State<T> : IEquatable<T>, IComparable<State<T>> where T : State<T>
+	{
+		public abstract int Cost { get; }
+
+		public abstract int CompareTo(State<T> other);
+
+		public abstract bool Equals(T other);
 	}
 }
--- src/Linnworks.CodingTests.Part3/Domain/Models.cs
+++ src/Linnworks.CodingTests.Part3/Domain/Models.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace Linnworks.CodingTests.Part3.Domain
 {
@@ -17,6 +18,55 @@ namespace Linnworks.CodingTests.Part3.Domain
 
 	public class World
 	{
+		public class CellState : State<CellState>
+		{
+			public CellState(Cell cell)
+			{
+				Location = cell;
+			}
+ 
+			public Cell Location { get; }
+			public int X => Location.X;
+			public int Y => Location.Y;
+			public override int Cost => Location.Passability;
+
+			public override bool Equals(object obj)
+			{
+				if (ReferenceEquals(null, obj)) return false;
+				if (ReferenceEquals(this, obj)) return true;
+				if (obj.GetType() != GetType()) return false;
+				return Equals(obj as CellState);
+			}
+
+			public override int GetHashCode()
+			{
+				unchecked
+				{
+					var hashCode = 13;
+					hashCode = (hashCode * 397) ^ Location.X;
+					hashCode = (hashCode * 397) ^ Location.Y;
+					return hashCode;
+				}
+			}
+
+			public override bool Equals(CellState other)
+			{
+				if (other == null) return false;
+				return Location.X == other.Location.X && Location.Y == other.Location.Y;
+			}
+
+			public override int CompareTo(State<CellState> other)
+			{
+				var otherCellState = other as CellState;
+				if (otherCellState.Location.X.CompareTo(Location.X) != 0)
+					return Location.X.CompareTo(otherCellState.Location.X);
+				else if (otherCellState.Location.Y.CompareTo(Location.Y) != 0)
+					return Location.Y.CompareTo(otherCellState.Location.Y);
+
+				return 0;
+			}
+		}
+
 		public class Cell : Location
 		{
 			// Defines cell passability from 0 (can't go) to 100 (normal passability)
@@ -46,38 +96,24 @@ namespace Linnworks.CodingTests.Part3.Domain
 		public Location[] FindShortestWay(Location startLoc, Location endLoc)
 		{
 			// TODO: Implement finding most shortest way between start and end locations
-			return new FindShorestPath(this.cells[startLoc.X, startLoc.Y], this.cells[endLoc.X, endLoc.Y], nextLoc =>
+			return new FindShorestPath<CellState>(new CellState(this.cells[startLoc.X, startLoc.Y]), new CellState(this.cells[endLoc.X, endLoc.Y]), currentState =>
 			{
+				var cellState = currentState as CellState;
 				var successors = new List<Cell>();
-				if (nextLoc.X + 1 < this.cells.GetLength(0))
-					successors.Add(this.cells[nextLoc.X + 1, nextLoc.Y]);
+				if (cellState.X + 1 < this.cells.GetLength(0))
+					successors.Add(this.cells[cellState.X + 1, cellState.Y]);
 
-				if (nextLoc.Y + 1 < this.cells.GetLength(1))
-					successors.Add(this.cells[nextLoc.X, nextLoc.Y + 1]);
+				if (cellState.Y + 1 < this.cells.GetLength(1))
+					successors.Add(this.cells[cellState.X, cellState.Y + 1]);
 
-				if (nextLoc.X - 1 >= 0)
-					successors.Add(this.cells[nextLoc.X - 1, nextLoc.Y]);
+				if (cellState.X - 1 >= 0)
+					successors.Add(this.cells[cellState.X - 1, cellState.Y]);
 
-				if (nextLoc.Y - 1 >= 0)
-					successors.Add(this.cells[nextLoc.X, nextLoc.Y - 1]);
+				if (cellState.Y - 1 >= 0)
+					successors.Add(this.cells[cellState.X, cellState.Y - 1]);
 
-				return successors;
-			}).Run();
-		}
-
-		public void Print() {
-
-			int rowLength = this.cells.GetLength(0);
-			int colLength = this.cells.GetLength(1);
-
-			for (int i = 0; i < rowLength; i++)
-			{
-				for (int j = 0; j < colLength; j++)
-				{
-					Console.Write(string.Format("{0} ", this.cells[i, j].Passability));
-				}
-				Console.Write(Environment.NewLine + Environment.NewLine);
-			}
+				return successors.Select(successor => new CellState(successor));
+			}).Run().OfType<CellState>().Select(cellState => cellState.Location).ToArray();
 		}
 	}
 }
