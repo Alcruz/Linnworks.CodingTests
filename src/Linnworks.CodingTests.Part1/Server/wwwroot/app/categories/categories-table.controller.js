fed48f0f74e16b8b1b9e5862ab510f991149c299
Alvin Jonel de la Cruz <alvinj.delacruz@gmail.com>
2018-08-18 21:52:32 -0400

    Create categories page

---
+++ src/Linnworks.CodingTests.Part1/Server/wwwroot/app/categories/categories-table.controller.js
@@ -0,0 +1,13 @@
+﻿(function() {
+
+	angular
+		.module("app")
+		.controller("CategoriesTableController", CategoriesTableController);
+	
+	/* recommended - but see next section */
+	function CategoriesTableController() {
+		this.name = {};
+		this.sendMessage = function() { };
+	}
+})();
+
