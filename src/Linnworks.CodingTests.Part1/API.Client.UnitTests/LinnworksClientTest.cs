77bac7eb0fda6a8929fde9fdf65baf645f4e6d8a
Alvin Jonel de la Cruz <alvinj.delacruz@gmail.com>
2018-08-21 22:22:50 -0400

    Add tests for create category

--- src/Linnworks.CodingTests.Part1/API.Client.UnitTests/LinnworksClientTest.cs
+++ src/Linnworks.CodingTests.Part1/API.Client.UnitTests/LinnworksClientTest.cs
@@ -33,16 +33,34 @@ namespace Linnworks.CodingTests.Part1.Server.API.Client.UnitTests
 		[Fact]
 		public async Task TestGetAllCategories()
 		{
-			const int expectedCategoriesCount = 4;
 			const string expectedFirstCategoryName = "Default";
 			const int expectedFirstProductCount = 10;
 
+			const string expectedLastCategoryName = "New Category";
+			const int expectedLastProductCount = 0;
+
 			var linnworksClient = new LinnworksClient(this.baseUrl, this.authSession);
 			var result = (await linnworksClient.GetCategories()).ToList();
-			
-			Assert.Equal(expectedCategoriesCount, result.Count);
-			Assert.Equal(expectedFirstCategoryName, result[0].Name);
-			Assert.Equal(expectedFirstProductCount, result[0].ProductsCount);
+			var defaultCategory = result.Find(r => r.Name == "Default");
+			var newCategory = result.Find(r => r.Name == "New Category");
+
+
+			Assert.Equal(linnworksApiMock.Categories.Count, result.Count);
+			Assert.Equal(expectedFirstCategoryName, defaultCategory.Name);
+			Assert.Equal(expectedFirstProductCount, defaultCategory.ProductsCount);
+			Assert.Equal(expectedLastCategoryName, newCategory.Name);
+			Assert.Equal(expectedLastProductCount, newCategory.ProductsCount);
+		}
+
+		[Theory]
+		[InlineData("Category1")]
+		[InlineData("Category2")]
+		public async Task TestAddValidCategory(string categoryName)
+		{
+			var linnworksClient = new LinnworksClient(this.baseUrl, this.authSession);
+			var category = await linnworksClient.CreateCategory(categoryName);
+
+			Assert.NotNull(linnworksApiMock.FindCategoryById(category.Id));
 		}
 	}
 }
